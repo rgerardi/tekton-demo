507d724ed85a79582d07289f941a9e9ff1943e92
Ricardo Gerardi <rgerardi@redhat.com>
2020-06-15 16:28:35 -0400

    Included simple test

--- main.go
+++ main.go
@@ -8,6 +8,14 @@ import (
 	"strconv"
 )
 
+func newMux() http.Handler {
+	m := http.NewServeMux()
+
+	m.HandleFunc("/", rootHandler)
+
+	return m
+}
+
 func rootHandler(w http.ResponseWriter, r *http.Request) {
 	log.Printf("%s request received: %s:%s:%s\n", r.Method, r.UserAgent(), r.URL, r.RemoteAddr)
 	hostname, err := os.Hostname()
@@ -20,21 +28,18 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-
 	port := 3000
 
 	if sp := os.Getenv("SERVER_PORT"); sp != "" {
-		p, err := strconv.Atoi(sp)
+		var err error
+		port, err = strconv.Atoi(sp)
 		if err != nil {
 			log.Fatal("Invalid port provided by env var SERVER_PORT: ", sp)
 		}
-		port = p
 	}
-
-	http.HandleFunc("/", rootHandler)
 
 	addr := fmt.Sprintf(":%d", port)
 	log.Printf("Starting server on address: %s\n\n", addr)
 
-	log.Fatal(http.ListenAndServe(addr, nil))
+	log.Fatal(http.ListenAndServe(addr, newMux()))
 }
---
+++ main_test.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRootHandler(t *testing.T) {
+	a := assert.New(t)
+
+	ts, cleanup := testServer(t)
+	defer cleanup()
+
+	hostname, err := os.Hostname()
+	a.NoError(err)
+
+	exp := fmt.Sprintf("This request is being served by sever %s\n", hostname)
+
+	res, err := http.Get(ts)
+	a.NoError(err)
+
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	a.NoError(err)
+
+	a.Equal(exp, string(body))
+}
+
+func testServer(t *testing.T) (string, func()) {
+	t.Helper()
+
+	s := httptest.NewServer(newMux())
+
+	return s.URL, func() {
+		s.Close()
+	}
+}
